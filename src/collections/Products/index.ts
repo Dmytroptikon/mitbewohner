e28f29700f0adadd16fc2e859e753af417f7f1d3
Oleksandra Ilienko <i.oleksandra.a@gmail.com>
2025-08-15 13:43:40 +0200

    add products

---
+++ src/collections/Products/index.ts
@@ -0,0 +1,31 @@
+import type { CollectionConfig } from 'payload'
+
+export const Products: CollectionConfig = {
+    slug: 'products',
+    admin: {
+        useAsTitle: 'name'
+    },
+  fields: [
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'units',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'quantity',
+      type: 'text',
+      required: true,
+    },
+  ],
+}
